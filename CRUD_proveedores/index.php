4cde4135163eedafb9f22b06edaf94285240e64a
julian duque <julianduquegarces@gmail.com>
2024-08-12 21:19:08 -0500

    Add files via upload

---
+++ CRUD_proveedores/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificacion</title>
+</head>
+<body>
+    holaa
+</body>
+</html>
+<?php
+header('location: proveedores.php')
+
+?>
